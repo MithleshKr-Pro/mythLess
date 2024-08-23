8aa26b7153fa11124280799540f7082224e2f67f
MithleshKr-Pro <coding4u2@gmail.com>
2024-08-23 20:20:53 +0530

    ReUpdated

--- script.js
+++ script.js
@@ -147,155 +147,169 @@
 
 //Chapter 5 Functions
 
-function myFunction(msg){
-    console.log("Welcome to Apna College!! ");
-    console.log("learning JS");
-    console.log(msg)
-}
-myFunction("I love Myself");
-myFunction(67);
+// function myFunction(msg){
+//     console.log("Welcome to Apna College!! ");
+//     console.log("learning JS");
+//     console.log(msg)
+// }
+// myFunction("I love Myself");
+// myFunction(67);
  
-//function for sum of 2 no.
-function sumOfNo(a,b){
-    console.log('Sum =',a+b);
-}
-sumOfNo(3,476);
-myFunction();
+// //function for sum of 2 no.
+// function sumOfNo(a,b){
+//     console.log('Sum =',a+b);
+// }
+// sumOfNo(3,476);
+// myFunction();
 
-//sum Function
-function sum(a,b){
-    return(a+b)
-}
-//arrowFunction
-const arrowSum=(a,b)=>{
-    console.log(a+b);
-}
-arrowSum(9,5)
-function mul(a,b){
-    return(a*b)
-}
-//arrowFunction modernJS
-const arrowMul=(a,b)=>{
-    console.log(a*b);
-}
-arrowMul(9,5);
+// //sum Function
+// function sum(a,b){
+//     return(a+b)
+// }
+// //arrowFunction
+// const arrowSum=(a,b)=>{
+//     console.log(a+b);
+// }
+// arrowSum(9,5)
+// function mul(a,b){
+//     return(a*b)
+// }
+// //arrowFunction modernJS
+// const arrowMul=(a,b)=>{
+//     console.log(a*b);
+// }
+// arrowMul(9,5);
 
-//practiceQ1
-function noOfVowels(string){
-    //let string=prompt("enter a word : ");
-    let vowels=('a','e','i','o','u');
-    let index=0;
-    let vowelno=0;
-    for (let index of string){
-        for(let indx of vowels){
-            if(vowels[indx]=!string[index])
-             {
-             vowelno++;
-             }
-            }
-        }
-        index++;
-        console.log(vowelno);
-    }
-noOfVowels('apple');
+// //practiceQ1
+// function noOfVowels(string){
+//     //let string=prompt("enter a word : ");
+//     let vowels=('a','e','i','o','u');
+//     let index=0;
+//     let vowelno=0;
+//     for (let index of string){
+//         for(let indx of vowels){
+//             if(vowels[indx]=!string[index])
+//              {
+//              vowelno++;
+//              }
+//             }
+//         }
+//         index++;
+//         console.log(vowelno);
+//     }
+// noOfVowels('apple');
 
-function countVowels(str){
-    let count=0;
-    for (let i of str){
-        if (i==="a"||i==="e"||i==="i"||i==="o"||i==="u"){
-            count++;
-        }
-    }
-    return(count);
-}
-//arrowFunction for same
-let countingVowels=(str)=>{
-    let count=0;
-    for (let i of str){
-        if (i==="a"||i==="e"||i==="i"||i==="o"||i==="u"){
-            count++;
-        }
-    }
-    return(count);
-}
+// function countVowels(str){
+//     let count=0;
+//     for (let i of str){
+//         if (i==="a"||i==="e"||i==="i"||i==="o"||i==="u"){
+//             count++;
+//         }
+//     }
+//     return(count);
+// }
+// //arrowFunction for same
+// let countingVowels=(str)=>{
+//     let count=0;
+//     for (let i of str){
+//         if (i==="a"||i==="e"||i==="i"||i==="o"||i==="u"){
+//             count++;
+//         }
+//     }
+//     return(count);
+// }
 
-let cities=["Pune","Mumbai","Delhi","Patna"];
-cities.forEach((val)=>{
-    console.log(val.toUpperCase());
-},2,cities)
-cities.forEach(function printVal(val){
-    console.log(val);
-})
-cities.forEach((val,id,cities)=>{
-    console.log(val.toUpperCase(),id,cities);
-})
+// let cities=["Pune","Mumbai","Delhi","Patna"];
+// cities.forEach((val)=>{
+//     console.log(val.toUpperCase());
+// },2,cities)
+// cities.forEach(function printVal(val){
+//     console.log(val);
+// })
+// cities.forEach((val,id,cities)=>{
+//     console.log(val.toUpperCase(),id,cities);
+// })
 
-//practiceQ 2
-noArray=[67,52,39];
-noArray.forEach((no,idx,noArray)=>{
-    console.log(idx,'Square=',no*no,noArray);
-})
-//other method to pass callback
-let calcSquare=(no,idx,noArray)=>{
-    console.log(idx,'Square=',no*no,noArray);
-}
-noArray.forEach(calcSquare);
-let newNoArray=noArray.map((no)=>{
-    console.log(no*no);
-    return (no*no);
-})
-console.log(noArray,newNoArray);
+// //practiceQ 2
+// noArray=[67,52,39];
+// noArray.forEach((no,idx,noArray)=>{
+//     console.log(idx,'Square=',no*no,noArray);
+// })
+// //other method to pass callback
+// let calcSquare=(no,idx,noArray)=>{
+//     console.log(idx,'Square=',no*no,noArray);
+// }
+// noArray.forEach(calcSquare);
+// let newNoArray=noArray.map((no)=>{
+//     console.log(no*no);
+//     return (no*no);
+// })
+// console.log(noArray,newNoArray);
 
-let num=[1,2,3,4,5,6,7,8,9,10];
-let evenNum=num.filter((val)=>{
-    return(val%2==0);
-})
-console.log(evenNum);
-//reduce Method For Array
-  //findAverage
-let arr=[5,6,2,1,3];
-const output=arr.reduce((prev,curr)=>{
-    return (prev+curr);
-})
-console.log(output/arr.length);
-//findGreatestVal
-let newOutput=arr.reduce((prev,curr)=>{
-    if(prev>=curr){
-        return prev;
-    }
-    else{
-        return curr;
-    }
-})
-console.log(newOutput);
-let newOutput1=arr.reduce((prev,curr)=>{
-    return prev<=curr ? prev:curr;
-})
-console.log(newOutput1);
-//practiceQ3
-studMarks=[87,93,64,99,86];
-let above90=studMarks.filter((val)=>{
-    return val>90 ;
-})
-console.log("Students Scoring 90+ = ",above90)
-//practiceQ4
-let n=[prompt("Enter a No. :")]
-let newArray=[]
-for(let i=1;i<=n;i++){
-    newArray[i-1]=i;
-}
-console.log(newArray);
+// let num=[1,2,3,4,5,6,7,8,9,10];
+// let evenNum=num.filter((val)=>{
+//     return(val%2==0);
+// })
+// console.log(evenNum);
+// //reduce Method For Array
+//   //findAverage
+// let arr=[5,6,2,1,3];
+// const output=arr.reduce((prev,curr)=>{
+//     return (prev+curr);
+// })
+// console.log(output/arr.length);
+// //findGreatestVal
+// let newOutput=arr.reduce((prev,curr)=>{
+//     if(prev>=curr){
+//         return prev;
+//     }
+//     else{
+//         return curr;
+//     }
+// })
+// console.log(newOutput);
+// let newOutput1=arr.reduce((prev,curr)=>{
+//     return prev<=curr ? prev:curr;
+// })
+// console.log(newOutput1);
+// //practiceQ3
+// studMarks=[87,93,64,99,86];
+// let above90=studMarks.filter((val)=>{
+//     return val>90 ;
+// })
+// console.log("Students Scoring 90+ = ",above90)
+// //practiceQ4
+// let n=[prompt("Enter a No. :")]
+// let newArray=[]
+// for(let i=1;i<=n;i++){
+//     newArray[i-1]=i;
+// }
+// console.log(newArray);
 //practiceQ5
-let sumArray=newArray.reduce((prev,curr)=>{
-    return prev+curr;
-})
-console.log('Sum = ',sumArray);
-//practicQ6
-let prodArray=newArray.reduce((prev,curr)=>{
-    return prev*curr;
-})
-console.log('factorial =',prodArray);
-let btn=document.querySelectorAll("button");
-console.log(btn);
-console.dir(btn);
-btn      
+// let sumArray=newArray.reduce((prev,curr)=>{
+//     return prev+curr;
+// })
+// console.log('Sum = ',sumArray);
+// //practicQ6
+// let prodArray=newArray.reduce((prev,curr)=>{
+//     return prev*curr;
+// })
+// console.log('factorial =',prodArray);
+// let btn=document.querySelectorAll("button");
+// console.log(btn);
+// console.dir(btn);
+// //document.body.style.backgroundColor="green";
+// console.dir(document.body.childNodes[3]);
+// console.dir(document.getElementsByClassName("signupbar"));
+// console.dir(window.document.getElementsByTagName("section"));
+// let elements=document.querySelector("a");
+
+// console.dir(elements);
+// console.dir(elements.tagName);
+// console.dir(elements.innerText);
+// console.dir(elements.innerHTML);
+// console.dir(document.querySelector("section").innerText);
+// console.dir(document.querySelector("section").innerHTML);
+// console.dir(document.querySelector("section").textContent);
+// console.dir(document.body.children);
+// elements.innerText="ABCDE"
